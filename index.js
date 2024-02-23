4abb4dd41e84f70b65ef891c815d9574b01c3c39
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2024-02-23 18:43:10 +0100

    make test, pass them

--- index.js
+++ index.js
@@ -34,17 +34,62 @@ export function Board() {
     ].filter(el => el !== null);
 
     return directions;
-  }
+  };
+
+  const isShipInDirection = ([row, col], direction, length) => {
+    switch (direction) {
+      case "up": {
+        for (let i = 0; i < length; i++) {
+          if (board[row - i][col] !== 0) {
+            return true;
+          }
+        }
+        break;
+      }
+      case "right": {
+        for (let i = 0; i < length; i++) {
+          if (board[row][col + i] !== 0) {
+            return true;
+          }
+        }
+        break;
+      }
+      case "down": {
+        for (let i = 0; i < length; i++) {
+          if (board[row + i][col] !== 0) {
+            return true;
+          }
+        }
+        break;
+      }
+      case "left": {
+        for (let i = 0; i < length; i++) {
+          if (board[row][col - i] !== 0) {
+            return true;
+          }
+        }
+        break;
+      }
+      default: return false;
+    }
+  };
 
   return {
     placeShip: ([row, col], ship, direction) => {
+      if (row < 0 || row > 9 || col < 0 || col > 9)
+        throw Error(`[row=${row},col=${col}] out of bounds!`);
+
       const isThereShip = board[row][col] !== 0;
-      if (isThereShip) throw Error(`There is a ship already at [${row}][${col}]`);
+      if (isThereShip)
+        throw Error(`There is a ship already at [${row}][${col}]`);
 
       const directions = getShipLegalDirections([row, col], ship);
-      if (!directions.includes(direction)) throw Error(`There is no space for a ship in "${direction}"`)
+      if (!directions.includes(direction))
+        throw Error(`There is no space for a ship in "${direction}"`)
 
-      // TODO: see if there are any ships in `direction` direction
+      const isShipOverlapping = isShipInDirection([row, col], direction, ship.length);
+      if (isShipOverlapping)
+        throw Error(`There is a ship towards ${direction} that causes overlapping`);
 
       switch (direction) {
         case "up": {
--- tests/Board.test.js
+++ tests/Board.test.js
@@ -1,8 +1,13 @@
 import { Board, Ship } from "../index.js";
 
+let board1;
+
+beforeEach(() => {
+  board1 = Board();
+});
+
 test("Board places ship of length 4 upwards", () => {
   const s1 = Ship(4);
-  const board1 = Board();
   board1.placeShip([5, 1], s1, "up");
   expect(board1.board[5][1]).toEqual(s1);
   expect(board1.board[4][1]).toEqual(s1);
@@ -12,7 +17,6 @@ test("Board places ship of length 4 upwards", () => {
 
 test("Board places ship of length 3 to the right", () => {
   const s1 = Ship(3);
-  const board1 = Board();
   board1.placeShip([2, 3], s1, "right");
   expect(board1.board[2][3]).toEqual(s1);
   expect(board1.board[2][4]).toEqual(s1);
@@ -21,7 +25,6 @@ test("Board places ship of length 3 to the right", () => {
 
 test("Board places ship of length 2 downwards", () => {
   const s1 = Ship(2);
-  const board1 = Board();
   board1.placeShip([4, 0], s1, "down");
   expect(board1.board[4][0]).toEqual(s1);
   expect(board1.board[5][0]).toEqual(s1);
@@ -29,10 +32,37 @@ test("Board places ship of length 2 downwards", () => {
 
 test("Board places ship of length 4 to the left", () => {
   const s1 = Ship(4);
-  const board1 = Board();
   board1.placeShip([8, 6], s1, "left");
   expect(board1.board[8][6]).toEqual(s1);
   expect(board1.board[8][5]).toEqual(s1);
   expect(board1.board[8][4]).toEqual(s1);
   expect(board1.board[8][3]).toEqual(s1);
+});
+
+test("Board throws error when placing ship in the same space", () => {
+  const s1 = Ship(2);
+  const s2 = Ship(3);
+  board1.placeShip([5, 5], s1, "up");
+
+  expect(() => board1.placeShip([5, 5], s2, "left")).toThrow(/already/);
+});
+
+test("Board handles ship outside the board", () => {
+  const s1 = Ship(2);
+  expect(() => board1.placeShip([12, 0], s1, "down")).toThrow(/out of bounds/);
+  expect(() => board1.placeShip([5, -1], s1, "up")).toThrow(/out of bounds/);
+});
+
+test("Board checks for empty space in board array", () => {
+  const s1 = Ship(3);
+  board1.placeShip([0, 0], s1, "right");
+  expect(board1.board[0][3]).toBe(0);
+});
+
+test("Board throws error when two ships are overlapping", () => {
+  const s1 = Ship(2);
+  const s2 = Ship(3);
+  board1.placeShip([4, 4], s1, "right");
+  const problemHere = () => board1.placeShip([5, 4], s2, "up");
+  expect(problemHere).toThrow(/overlapping/)
 });
