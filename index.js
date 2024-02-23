d089da6dc5ccf22fc31fe7bf4dfb0a2886f91eff
Kiril Hizhavsky <79530158+qirele@users.noreply.github.com>
2024-02-23 15:42:02 +0100

    add placement tests & implementation

--- index.js
+++ index.js
@@ -14,10 +14,67 @@ export function Ship(length) {
   }
 }
 
-export function Board(board, ships) {
+export function Board() {
+  let board = [];
+  for (let i = 0; i < 10; i++) {
+    let arr = Array(10).fill(0);
+    board.push(arr);
+  }
+
+  const getShipLegalDirections = ([row, col], ship) => {
+    const hasSpaceUp = (row - ship.length) >= 0;
+    const hasSpaceRight = (col + ship.length) <= 9;
+    const hasSpaceDown = (row + ship.length) <= 9;
+    const hasSpaceLeft = (col - ship.length) >= 0;
+    const directions = [
+      hasSpaceUp ? "up" : null,
+      hasSpaceRight ? "right" : null,
+      hasSpaceDown ? "down" : null,
+      hasSpaceLeft ? "left" : null,
+    ].filter(el => el !== null);
+
+    return directions;
+  }
+
   return {
-    receiveAttack: ([row, col]) => {
-      board[row][col].hit();
+    placeShip: ([row, col], ship, direction) => {
+      const isThereShip = board[row][col] !== 0;
+      if (isThereShip) throw Error(`There is a ship already at [${row}][${col}]`);
+
+      const directions = getShipLegalDirections([row, col], ship);
+      if (!directions.includes(direction)) throw Error(`There is no space for a ship in "${direction}"`)
+
+      // TODO: see if there are any ships in `direction` direction
+
+      switch (direction) {
+        case "up": {
+          for (let i = 0; i < ship.length; i++) {
+            board[row - i][col] = ship;
+          }
+          break;
+        }
+        case "right": {
+          for (let i = 0; i < ship.length; i++) {
+            board[row][col + i] = ship;
+          }
+          break;
+        }
+        case "down": {
+          for (let i = 0; i < ship.length; i++) {
+            board[row + i][col] = ship;
+          }
+          break;
+        }
+        case "left": {
+          for (let i = 0; i < ship.length; i++) {
+            board[row][col - i] = ship;
+          }
+          break;
+        }
+      }
     },
+    get board() {
+      return board;
+    }
   }
 }
--- tests/Board.test.js
+++ tests/Board.test.js
@@ -1,28 +1,38 @@
 import { Board, Ship } from "../index.js";
 
-const s1 = Ship(4);
-const s2 = Ship(3);
-const s3 = Ship(3);
-const s4 = Ship(2);
-const s5 = Ship(2);
-const s6 = Ship(2);
+test("Board places ship of length 4 upwards", () => {
+  const s1 = Ship(4);
+  const board1 = Board();
+  board1.placeShip([5, 1], s1, "up");
+  expect(board1.board[5][1]).toEqual(s1);
+  expect(board1.board[4][1]).toEqual(s1);
+  expect(board1.board[3][1]).toEqual(s1);
+  expect(board1.board[2][1]).toEqual(s1);
+});
 
-const mockBoard = [
-  [s3, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [s3, 0, 0, 0, 0, 0, 0, 0, s2, 0,],
-  [s3, 0, 0, 0, s6, 0, 0, 0, s2, 0,],
-  [0, 0, 0, 0, s6, 0, 0, 0, s2, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, s1, s1, s1, s1, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, s4, 0, 0, 0, 0, 0, s5, s5, 0,],
-  [0, s4, 0, 0, 0, 0, 0, 0, 0, 0,],
-]
+test("Board places ship of length 3 to the right", () => {
+  const s1 = Ship(3);
+  const board1 = Board();
+  board1.placeShip([2, 3], s1, "right");
+  expect(board1.board[2][3]).toEqual(s1);
+  expect(board1.board[2][4]).toEqual(s1);
+  expect(board1.board[2][5]).toEqual(s1);
+});
 
-test("Board receives attack, calls hit() on the proper ship", () => {
-  const board1 = Board(mockBoard);
-  const spy = jest.spyOn(s1, "hit");
-  board1.receiveAttack([5, 1]);
-  expect(spy).toHaveBeenCalled();
+test("Board places ship of length 2 downwards", () => {
+  const s1 = Ship(2);
+  const board1 = Board();
+  board1.placeShip([4, 0], s1, "down");
+  expect(board1.board[4][0]).toEqual(s1);
+  expect(board1.board[5][0]).toEqual(s1);
+});
+
+test("Board places ship of length 4 to the left", () => {
+  const s1 = Ship(4);
+  const board1 = Board();
+  board1.placeShip([8, 6], s1, "left");
+  expect(board1.board[8][6]).toEqual(s1);
+  expect(board1.board[8][5]).toEqual(s1);
+  expect(board1.board[8][4]).toEqual(s1);
+  expect(board1.board[8][3]).toEqual(s1);
 });
